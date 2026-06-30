8d993c0a07963c133683f71af83c68c940c5b637
onidzuka <onidzukatiqer@gmail.com>
2026-06-30 16:46:57 +0300

    test again

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ set -e
 echo "Updating backend..."
 cd /opt/spring-lab/spring-backend-lab
 git fetch origin master
-git reset --hard origin/main
+git reset --hard origin/master
 
 echo "Updating bff..."
 cd /opt/spring-lab/bff
 git fetch origin master
-git reset --hard origin/main
+git reset --hard origin/master
 
 echo "Rebuilding containers..."
 docker compose -f docker-compose.prod.yml up -d --build
