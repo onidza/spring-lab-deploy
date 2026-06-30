3bc8f53fd5e8c38d86d95fe544d1525877ba5406
onidzuka <onidzukatiqer@gmail.com>
2026-06-30 16:42:19 +0300

    test again

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "Updating backend..."
 cd /opt/spring-lab/spring-backend-lab
-git fetch origin master
+git fetch origin main
 git reset --hard origin/main
 
 echo "Updating bff..."
 cd /opt/spring-lab/bff
-git fetch origin master
+git fetch origin main
 git reset --hard origin/main
 
 echo "Rebuilding containers..."
